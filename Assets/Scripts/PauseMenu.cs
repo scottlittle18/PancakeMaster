65375d13f5ee0a2ece26aedda4e65c36b0887810
bpaner21 <bernardjohn.paner@loop.colum.edu>
2018-10-12 18:24:01 -0500

    Added GUI Prefabs and code

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    private bool paused = false;
+    public GameObject PauseUI;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PauseUI.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+
+            if (paused)
+            {
+                PauseUI.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                PauseUI.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        paused = false;
+        PauseUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
