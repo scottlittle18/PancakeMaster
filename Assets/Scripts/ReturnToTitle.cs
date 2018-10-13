0720c67c3aaee96de3ffe89a98c2f8a13e63526a
bpaner21 <bernardjohn.paner@loop.colum.edu>
2018-10-13 13:48:12 -0500

    Created object to exit from Credits to Title Screen

---
+++ Assets/Scripts/ReturnToTitle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour
+{
+	void Update ()
+    {
+		if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Title Screen");
+        }
+	}
+}
