dbaba354210b71bb3c67e411401d49e242a3fadc
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-10-13 13:35:18 -0500

    Added Crumble Prefab

---
+++ Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    [SerializeField]
+    private int FallDelay = 3;
+    [SerializeField]
+    private int FallSpeed = 5;
+
+    private bool isFalling;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            StartCoroutine("CrumbleDelay");
+        }
+    }
+    public IEnumerator CrumbleDelay()
+    {
+        yield return new WaitForSeconds(FallDelay);
+        this.GetComponent<BoxCollider2D>().enabled = false;
+        isFalling = true;
+    }
+    private void Update()
+    {
+        if(isFalling)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * FallSpeed);
+        }
+    }
+}
