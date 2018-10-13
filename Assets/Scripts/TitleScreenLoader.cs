45859864c224ca7af8b0266dcdb9895bf7c6b1f7
bpaner21 <bernardjohn.paner@loop.colum.edu>
2018-10-13 12:08:17 -0500

    Basic Title Screen. Play defaults to Level 1, but that can be changed pretty easily

---
+++ Assets/Scripts/TitleScreenLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleScreenLoader : MonoBehaviour
+{
+    public void LoadTest()
+    {
+        SceneManager.LoadScene("Test Level");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+	
+}
