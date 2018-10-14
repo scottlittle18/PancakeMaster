942fae49363c999ddd84abb051e6ac9f2d0dd75f
Scott Little <scott.little@loop.colum.edu>
2018-10-14 18:34:12 -0500

    title screen menu works
    - title menu works smoothly and the script has had an QuitGame() function added to handle the quit game button using the Application.Quit() function

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,76 +6,45 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    int currentScene;
-    bool onPlay, onCredits;
 
-    MouseCursor currentPosition;    
-
-    LayerMask uiPlay, uiCredits;
-
-    private void Start()
-    {
-        currentScene = SceneManager.GetActiveScene().buildIndex;
-        
-    }
-
-    private void Update()
-    {
-        onPlay = Physics2D.OverlapCircle(currentPosition.transform.position, currentPosition.cursorSensitivity, uiPlay);
-        onCredits = Physics2D.OverlapCircle(currentPosition.transform.position, currentPosition.cursorSensitivity, uiCredits);
-
-        if (onPlay && Input.GetButton("Fire1"))
-            PlayGame();
-        else if (onCredits && Input.GetButton("Fire1"))
-            PlayCredits();
-
-
-
-    }
-
-    private void PlayGame()
-    {
-        
-    }
-
-    private void PlayCredits()
-    {
-        SceneManager.LoadScene(5);
-        
-    }
-    /***
-        public void LoadTest()
+        public void TestLevel()
         {
             SceneManager.LoadScene("Test Level");
         }
+    
         public void LoadTitleScreen()
         {
             SceneManager.LoadScene("Title Screen");
         }
 
-        public void LoadLevel1()
+        public void Level1()
         {
             SceneManager.LoadScene("Level 1");
         }
 
-        public void LoadLevel2()
+        public void Level2()
         {
             SceneManager.LoadScene("Level 2");
         }
 
-        public void LoadLevel3()
+        public void Level3()
         {
             SceneManager.LoadScene("Level 3");
         }
 
-        public void LoadLevel4()
+        public void Level4()
         {
             SceneManager.LoadScene("Level 4");
         }
 
-        public void LoadCredits()
+        public void Credits()
         {
             SceneManager.LoadScene("Credits");
         }
-        ***/
+
+        public void QuitGame()
+        {        
+            Application.Quit();
+        }
+
 }
